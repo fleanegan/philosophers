9a0ba2d82df1d9896d0fa3912f71fa94a3a76c52
Frederik SCHLUTER <fschlute@e1r11p17.42.fr>
2022-04-04 17:58:02 +0200

    fix function oder in utils

--- src/utils/itoa_utils.c
+++ src/utils/itoa_utils.c
@@ -12,25 +12,6 @@
 
 #include "../philosophers.h"
 
-char	*ft_itoa(int n)
-{
-	char			*result;
-	int				digits_to_hold_number;
-	unsigned long	tmp;
-
-	tmp = n;
-	if (n < 0)
-		tmp = -n;
-	digits_to_hold_number = calc_digits_in_number(n);
-	result = allocate_memory_and_zero_init(&result, digits_to_hold_number);
-	if (! result)
-		return (NULL);
-	translate_int_to_char_array(tmp, result, digits_to_hold_number);
-	if (n < 0)
-		result[0] = '-';
-	return (result);
-}
-
 char	*allocate_memory_and_zero_init(\
 			char **result, int digits_to_hold_number)
 {
@@ -77,4 +58,23 @@ void	translate_int_to_char_array(unsigned int n, char *result,
 		result[current_index--] = '0' + (n % 10);
 		n = n / 10;
 	}
+}
+
+char	*ft_itoa(int n)
+{
+	char			*result;
+	int				digits_to_hold_number;
+	unsigned long	tmp;
+
+	tmp = n;
+	if (n < 0)
+		tmp = -n;
+	digits_to_hold_number = calc_digits_in_number(n);
+	result = allocate_memory_and_zero_init(&result, digits_to_hold_number);
+	if (! result)
+		return (NULL);
+	translate_int_to_char_array(tmp, result, digits_to_hold_number);
+	if (n < 0)
+		result[0] = '-';
+	return (result);
 }
